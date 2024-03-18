fb7f2679e76ef3ba07c92257e454ab732db6cde3
jredisme <jared.inbox@gmail.com>
2024-03-18 15:52:48 -0600

    Revert "Default colors and ability to change colors added to GUI"

--- code/memory.py
+++ code/memory.py
@@ -1,6 +1,6 @@
 class Memory():
     '''Memory class, handles memory-related operations'''
-    def __init__(self, size=250):
+    def __init__(self, size=100):
         self._registers = [0] * size
     
     def load(self, operand):
@@ -17,19 +17,15 @@ class Memory():
             self._registers[i] = self.truncate(instruction)  # avoid overflow
     
     def truncate(self, value):
-        # truncate value to six digits to avoid overflow when needed  
-        if value > 999999:
-            value %= 1000000
-        elif value < -999999:
+        # truncate value to four digits to avoid overflow when needed  
+        if value > 9999:
+            value %= 10000 
+        elif value < -9999:
             value = -value
-            value %= 1000000
+            value %= 10000
             value = -value
         return value
     
     def len(self):
         # return length of registers list
         return len(self._registers)
-    
-    def clear(self):
-        # clear memory
-        self._registers = [0] * self.len()
