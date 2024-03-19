1fe6de2a83cd4a342682ebb63c6ca78978fac89f
jredisme <jared.inbox@gmail.com>
2024-03-18 19:19:58 -0600

    Update GUI.py

--- code/GUI.py
+++ code/GUI.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-from tkinter import colorchooser
 from execute_program import Execute
 from process_program import Process
 
@@ -11,53 +10,52 @@ class SimpleGUI:
         self.main = tk.Tk()  # create the main gui window
         self.main.title("UVSIM")  # title of main window
         self.main.geometry("500x500") # dimensions of main window
-        self.main.configure(bg="#4C721D")  # main window color
-        
+        self.main.configure(bg="lightblue")  # main window color
         self.sim = sim
-
-        self.menu_bar = tk.Menu(self.main)  # create menu bar
-
-
         self.memory = memory
-        
-        # label for main window with initial file select message
-        self.label = tk.Label(self.main, text="Welcome to the UVUSIM! Please select a text file to run:")
-        self.label.pack(pady=10)
 
+        # create menu bar
+        self.menu_bar = tk.Menu(self.main)
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)  # create file menu
-        self.file_menu.add_command(label="Open", command=self.select_file)  # call select_file
-        self.file_menu.add_command(label="Save", command=self.save_file)  # call save_file
-        self.file_menu.add_command(label="Run", command=self.load_file)  # call load_file
-        self.file_menu.add_command(label="Configure Color Scheme", command=self.configure_color_scheme)
-
+        self.file_menu.add_command(label="Open", command=self.open_file)  # call open_file
+        self.file_menu.add_command(label="Save Code Block", command=self.save_code_block)  # call save_code_block
+        self.file_menu.add_command(label="Run Code Block", command=self.run_code_block)  # call run_code_block
         self.menu_bar.add_cascade(label="File", menu=self.file_menu)  # add file menu to menu bar
         self.main.config(menu=self.menu_bar)  # add menu bar to main window  # put text widget in block
-        # # file select submit button
-        # self.file_button = tk.Button(self.main, text="Select File", command=self.select_file) # call select_file
-        # self.file_button.pack()  # put button in block
-        # self.save_button = tk.Button(self.main, text="Save", command=self.save_file) # call save_file
-        # self.file_button.pack()  # put button in block
-        # self.run_button = tk.Button(self.main, text="Run", command=self.load_file) # call load_file
-        # self.file_button.pack()  # put button in block
+        
+        # label for main window with initial file select message
+        self.label_1 = tk.Label(self.main, text="Welcome to the UVUSIM! Please select a text file to run:")
+        self.label_1.pack(pady=10)
 
-        self.exit_button = tk.Button(self.main, text="Exit", command=self.destroy_program) # call main.destroy
-        self.exit_button.pack(side=tk.BOTTOM, pady=10)
-        self.exit_button.config(height=2, width=10)
-
+        # labels for code block and code text
         self.label_2 = tk.Label(self.main, text="UVSim code block")
         self.code_text = tk.Text(self.main, height=10, width=40)
+        self.label_2.pack(pady=10)
         self.code_text.pack(pady=10)
+
         # output sim operation log to user
-        self.label = tk.Label(self.main, text="UVSim operations log")
-        self.operations_text = tk.Text(self.main, height=10, width=40)
+        self.label_3 = tk.Label(self.main, text="UVSim operations log")
+        self.operations_text = tk.Text(self.main, height=10, width=40, state=tk.DISABLED)  #Read-only
+        self.label_3.pack(pady=10)
         self.operations_text.pack(pady=10)
 
-        self.operations_text.configure(bg="#FFFFFF") #texbox color
-        self.code_text.configure(bg="#FFFFFF") #textbox color
-
         self._program = []  # initialize empty program
-        
-    def save_file(self):
+
+    def open_file(self):
+        # search directories and choose a txt file
+        file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+        self.code_text.delete(1.0, tk.END)  # clear previous input
+        if file_path:
+            try:                         
+                program = Process.read_txt(file_path)
+                for line in program:
+                    self.code_text.insert(tk.END, f"{line}\n")
+            except Exception as e:
+                messagebox.showerror("Error", str(e))
+        else:
+            messagebox.showinfo("Info", "No file selected.")   
+
+    def save_code_block(self):
         file_path = filedialog.asksaveasfilename(filetypes=[("Text files", "*.txt")])
         if file_path:
             try:
@@ -67,62 +65,22 @@ class SimpleGUI:
             except Exception as e:
                 messagebox.showerror("Error", str(e))
 
-    def select_file(self):
-        # search directories and choose a txt file
-        file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
-        if file_path:
-                with open(file_path, 'r') as file:
-                    for line in file:
-                        self._program.append(int(line.strip()))  # add each line of program to program, check for int
-                        self.code_text.insert(tk.END, f"{line}")  # output program to gui
-            
-        else:
-            messagebox.showinfo("Info", "No file selected.")
-    
-    def load_file(self):
-        self._program = [int(line.strip()) for line in self.code_text.get(1.0, tk.END).split("\n") if line.strip()]
-                # load program from the user-selected file into the sim
-
-        self.sim.load_ml_program(self._program)  # load program into sim
-        Execute.execute_program(self.sim, self)  # execute program with Execute class
-        self.final_output()  # output accumulator value in gui
-
-        '''try:                         
-            program = Process.read_txt(file_path)  # read program with Process class
-            self.memory.load_program(program)  # load program into memory
+    def run_code_block(self):
+        program = [int(line.strip()) for line in self.code_text.get(1.0, tk.END).split("\n") if line.strip()]
+        try:
+            self.memory.load_program(program)   # load program from the user text input into the sim
             Execute.execute_program(self.sim, self, self.memory)  # execute program with Execute class
             self.final_output()  # output accumulator value in gui
-            self.main.destroy()  # exit gui
         except Exception as e:
             messagebox.showerror("Error", str(e))
-        else:
-            messagebox.showinfo("Info", "No file selected.")'''
-        
-    def configure_color_scheme(self):
-        # Function to configure color scheme
-        primary_color = colorchooser.askcolor(title="Choose Primary Color")[1]
-        off_color = colorchooser.askcolor(title="Choose Off Color")[1]
-
-        # Apply the chosen colors to the GUI
-        self.main.configure(bg=primary_color)
-        self.label.configure(bg=primary_color)
-        self.code_text.configure(bg=off_color)
-        self.operations_text.configure(bg=off_color)
-
-    def operations_output(self, op, func_name, operand):
-        # output accumulator value in gui
-        output = f'Performed op {op}: {func_name} with operand {operand}\n'
-        self.operations_text.insert(tk.END, output)
 
     def final_output(self):
-        # output accumulator value in gui
+        # output accumulator value to gui
         output = f"Final accumulator value: {self.sim._accumulator}\n"
+        self.operations_text.config(state=tk.NORMAL)
         self.operations_text.insert(tk.END, output)
+        self.operations_text.config(state=tk.DISABLED)
         messagebox.showinfo("Result: ", f"Final Accumulator Value: {self.sim._accumulator}") 
-    
-    def destroy_program(self):
-        # close the main window
-        self.main.destroy()
 
     def read(self):
         #Read a word from the keyboard into memory
@@ -133,7 +91,7 @@ class SimpleGUI:
         def submit():
             try:
                 value = int(entry.get())  #check for int
-                self.memory.truncate(value)  #truncate if the user value exceeds 4 digits
+                value = self.memory.truncate(value)  #truncate if the user value exceeds 4 digits
                 self.memory._registers[self.sim._operand] = value  # place user input into memory
                 entry_window.destroy()  # close user input window
             except ValueError:
@@ -152,7 +110,10 @@ class SimpleGUI:
     def write(self):
         # write a word from memory to gui
         value = self.memory._registers[self.sim._operand]
-        messagebox.showinfo("Write Operation", f"Value at memory location {self.sim._operand}: {value}")
+        output = f"Write Op: Value at register {self.sim._operand}: {value}\n"
+        self.operations_text.config(state=tk.NORMAL)
+        self.operations_text.insert(tk.END, output)
+        self.operations_text.config(state=tk.DISABLED)
 
     def too_long(self):
         # gui error message if sim pc exceeds available memory
