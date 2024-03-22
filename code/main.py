dacb016e924ac1d704ac45969c212b968c12d167
jredisme <jared.inbox@gmail.com>
2024-03-22 14:00:35 -0600

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,13 +1,13 @@
 from uvsim import UVSim
-from GUI import SimpleGUI
 from memory import Memory
+from GUI_actions import GUIActions
 
 def main():
     '''Run machine language simulator'''
     
     my_sim = UVSim()  # initialize simulator
     my_memory = Memory(100)  # initialize Memory object with 100 registers  
-    my_gui = SimpleGUI(my_sim, my_memory) # intialize gui, pass sim and memory to gui
+    my_gui = GUIActions(my_sim, my_memory) # perform GUI actions
     my_gui.main.mainloop() #loops through GUI operations while GUi is open
 
 if __name__ == "__main__":
